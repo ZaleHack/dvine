ca795d45ed692c57db57fac95f97a8f239578ea6
ZaleHack <saliou.thiam@ism.edu.sn>
2025-11-22 12:16:12 +0000

    Use autres realtime CDR table for geolocation

--- server/services/StatsService.js
+++ server/services/StatsService.js
@@ -13,7 +13,7 @@ import TransactionAnalysisService from './TransactionAnalysisService.js';
 import CallAnalysisService from './CallAnalysisService.js';
 
 const EXCLUDED_DATA_TABLES = new Set(
-  ['di_autres.cdr_temps_reel', 'cdr_temps_reel'].map((name) => name.toLowerCase())
+  ['autres.cdr_temps_reel', 'cdr_temps_reel'].map((name) => name.toLowerCase())
 );
 
 const realtimeDataExclusions = getRealtimeCdrTableIdentifiers();
